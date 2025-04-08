4889728b897a36158810d405ad2ba3da40017a3f
MaiDeme <maiwen.demeulle@gmail.com>
2025-04-08 14:10:17 +0200

    Update .gitignore, add new images, and modify Snakemake workflow and config for improved directory structure and functionality

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-snakemake --rulegraph | dot -Tpng > images/dag.png
+snakemake --configfile test/config_test.yaml --use-conda --rulegraph | dot -Tpng > images/rulegraph.png
+snakemake --configfile test/config_test.yaml --use-conda --dag | dot -Tpng > images/dag.png
+snakemake --configfile test/config_test.yaml --use-conda --filegraph | dot -Tpng > images/filegraph.png
+snakemake --configfile test/config_test.yaml --use-conda
