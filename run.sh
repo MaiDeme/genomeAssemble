85347f4c8b0f790eb3d7906222a3865fa27abd74
MaiDeme <maiwen.demeulle@gmail.com>
2025-04-10 11:34:52 +0200

    Enhance Snakemake workflow: add compress_fastq_pod5 rule, update Dockerfiles for NextPolish and funannotate, modify config paths, and clean up run script

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 snakemake --configfile test/config_test.yaml --use-conda --rulegraph | dot -Tpng > images/rulegraph.png
-snakemake --configfile test/config_test.yaml --use-conda --dag | dot -Tpng > images/dag.png
+#snakemake --configfile test/config_test.yaml --use-conda --dag | dot -Tpng > images/dag.png
 snakemake --configfile test/config_test.yaml --use-conda --filegraph | dot -Tpng > images/filegraph.png
-snakemake --configfile test/config_test.yaml --use-conda
