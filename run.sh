2cc8ab2c5bfc6dfb84cd935a11bb96fb6d17a66c
MaiDeme <maiwen.demeulle@gmail.com>
2025-04-14 16:08:38 +0200

    shoudl be working now

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-snakemake --configfile test/config_test.yaml --use-conda --rulegraph | dot -Tpng > images/rulegraph.png
-#snakemake --configfile test/config_test.yaml --use-conda --dag | dot -Tpng > images/dag.png
-snakemake --configfile test/config_test.yaml --use-conda --filegraph | dot -Tpng > images/filegraph.png
+#snakemake --configfile test/config_test.yaml --use-conda --rulegraph | dot -Tpng > images/rulegraph.png
+#snakemake --configfile test/config_test.yaml --use-conda --filegraph | dot -Tpng > images/filegraph.png
+#snakemake --configfile test/config_test.yaml --use-conda --use-singularity
+
+snakemake --use-conda --rulegraph | dot -Tpng > images/rulegraph.png
+snakemake --use-conda --filegraph | dot -Tpng > images/filegraph.png
+snakemake --use-conda --use-singularity --cores 10
